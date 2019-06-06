c5476a85fa5000dca487044695d184c04e74e1bb
Peter Petrov <peter.petrov@endava.com>
2019-06-06 13:58:43 +0300

    Removed "About" info

--- LMS/LMS/App_Start/BundleConfig.cs
+++ LMS/LMS/App_Start/BundleConfig.cs
@@ -23,6 +23,7 @@ namespace LMS
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
